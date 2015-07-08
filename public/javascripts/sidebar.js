d9f0fd5430c3a6ed96e1c6c0c8d6219906408255
noel9999 <terrorer9999@gmail.com>
2015-07-08 19:30:21 +0800

    讓siderbar滑動後固定位置，並拿掉sidebar裡的github顯示

--- public/javascripts/sidebar.js
+++ public/javascripts/sidebar.js
@@ -20,3 +20,30 @@ $('.sidebar').hover(
       );
     }
   );
+
+
+$(function(){
+  // 先取得 #cart 及其 top 值
+  var $sidebar = $('.sidebar'),
+    _top = $sidebar.offset().top;
+
+  // 當網頁捲軸捲動時
+  var $win = $(window).scroll(function(){
+    // 如果現在的 scrollTop 大於原本 #sidebar 的 top 時
+    if($win.scrollTop() >= _top){
+      // 如果 $sidebar 的座標系統不是 fixed 的話
+      if($sidebar.css('position')!='fixed'){
+        // 設定 #sidebar 的座標系統為 fixed
+        $sidebar.css({
+          position: 'fixed',
+          top: 0
+        });
+      }
+    }else{
+      // 還原 #sidebar 的座標系統為 absolute
+      $sidebar.css({
+        position: 'absolute'
+      });
+    }
+  });
+});
